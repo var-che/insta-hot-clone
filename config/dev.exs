07f83d582d5ddbf776eb7857f5235898f78b8591
Vladimir <filo.vlada@gmail.com>
2018-03-20 18:43:49 +0100

    Sucesfull absinthe configuration, and made first query

--- config/dev.exs
+++ config/dev.exs
@@ -40,7 +40,7 @@ config :phoenix, :stacktrace_depth, 20
 config :instagram, Instagram.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "afloderm",
   database: "instagram_dev",
   hostname: "localhost",
   pool_size: 10
---
+++ lib/instagram_web/schema.ex
@@ -0,0 +1,13 @@
+defmodule InstagramWeb.Schema do
+    use Absinthe.Schema
+
+    alias InstagramWeb.Resolvers
+
+    import_types __MODULE__.PostsTypes
+    query do
+        @desc "Get list of photos, this confirmed."
+        field :photos, list_of(:photo) do
+            resolve &Resolvers.Posts.photos/3
+        end
+    end
+end
